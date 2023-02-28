f715d2d776d745b7eecde667b8008e4aa15d5d4a
Kaiter Wu <wukaiter@gmail.com>
2023-02-28 17:19:28 -0500

    finishing exponent recursion

--- skeleton/phase_4_recursion.js
+++ skeleton/phase_4_recursion.js
@@ -17,3 +17,24 @@ const sumRec = function(arr) {
 
   return sum + sumRec(arr)
 }
+
+const exponent = (b,n) => {
+  if (n===0) {
+    return 1
+  }
+  return b*exponent(b,n-1)
+}
+
+const exponent2 = (b,n) => {
+  if (n===0){
+    return 1 
+  } else if (n===1) {
+    return b 
+  }
+
+  if (n % 2 === 0) { 
+    return exponent(b,n/2)**2
+  } else {
+    return (exponent(b,(n-1)/2)**2)*b
+  }
+}
