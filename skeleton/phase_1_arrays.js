2f8fd62460d5b62f7677ebac699bef339d91f784
Sean Abbas <sean.abbas33@gmail.com>
2023-02-28 14:46:13 -0500

    twosum

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -8,3 +8,25 @@
     }
     return  outputArray
  }
+
+ Array.prototype.twosum = function() {
+    let outputArr = []
+
+    for (let i = 0; i < this.length; i++) {
+        let ele1 = this[i]
+
+        for (let j = i + 1; j < this.length; j++) {
+            let ele2 = this[j]
+
+            if (ele1 + ele2 === 0) {
+                outputArr.push([i, j])
+            }
+        }
+
+    }
+    return outputArr
+ }
+
+ arr = [1, 2, 3, -1, -3, -2, 10, 0, 0]
+
+ console.log(arr.twosum())
